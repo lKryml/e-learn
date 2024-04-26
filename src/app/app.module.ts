9004aedb3a370ec9d1384a066135d124a4c4c705
Krym <gumimash@gmail.com>
2024-04-26 07:23:25 +0200

    removed taiga-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { TuiRootModule, TuiDialogModule, TuiAlertModule } from '@taiga-ui/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TuiButtonModule } from '@taiga-ui/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,10 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    TuiRootModule,
-    TuiDialogModule,
-    TuiAlertModule,
-    TuiButtonModule,
+
     NavbarComponent,
   ],
   providers: [],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [TuiButtonModule],
+  imports: [],
 })
 export class NavbarComponent {}
